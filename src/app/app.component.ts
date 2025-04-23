dfed429eeb6ee8f0d4a2ca86f3c5bd563e377ad1
haji <muhamedhajajii@outlook.com>
2025-04-24 00:22:09 +0200

    dist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-
+import { FlowbiteService } from './core/services/flowbite.service';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink],
@@ -8,4 +8,11 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  constructor(private flowbiteService: FlowbiteService) {}
+
+  ngOnInit() {
+    this.flowbiteService.loadFlowbite((flowbite) => {
+      flowbite.default.initFlowbite();
+    });
+  }
 }
